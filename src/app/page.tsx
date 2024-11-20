44ce6a2459a55e46250e52bc833d35a745a87869
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-20 14:09:26 +0000

    add sign in / sign out buttons to home page and show user avatar when signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
-import { Button } from '@nextui-org/react';
-export default function Home() {
+import { Button, Image } from '@nextui-org/react';
+import * as actions from '@/actions';
+import { auth } from '@/auth';
+
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Click Me</Button>
+      {session?.user ? (
+        <div
+          className="flex items-center gap-2"
+          title={JSON.stringify(session.user)}>
+          <Image
+            src={session.user.image as string}
+            className="size-10 rounded-full"
+          />
+          Welcome {session.user.name}
+        </div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
     </div>
   );
 }
