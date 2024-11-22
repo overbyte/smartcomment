f0412c62f42888743733a4186c7c26bdf3e99c67
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-22 11:46:42 +0000

    factor auth from server component to client component to stop headers causing all pages to become dynamic (ie fix caching on homepage)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,58 +5,10 @@ import {
   NavbarContent,
   NavbarItem,
   Input,
-  Button,
-  Avatar,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
 } from '@nextui-org/react';
-import { auth } from '@/auth';
-import * as actions from '@/actions';
+import HeaderAuth from '@/components/header-auth';
 
-export default async function Header() {
-  const session = await auth();
-
-  const getAuthContent = (): React.ReactNode => {
-    if (session?.user) {
-      return (
-        <Popover placement="left">
-          <PopoverTrigger>
-            <Avatar src={session.user.image ?? ''} />
-          </PopoverTrigger>
-
-          <PopoverContent>
-            <div className="p-4">
-              <form action={actions.signOut}>
-                <Button type="submit">Sign Out</Button>
-              </form>
-            </div>
-          </PopoverContent>
-        </Popover>
-      );
-    }
-
-    return (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" variant="flat">
-              Sign Up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  };
-
+export default function Header() {
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -71,7 +23,9 @@ export default async function Header() {
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent justify="end">{getAuthContent()}</NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
