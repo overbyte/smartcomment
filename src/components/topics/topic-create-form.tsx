6879e27ca6d8de9c6a0c0e95beda552c384c6e48
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-22 13:37:33 +0000

    switch to onSubmit from form action and then use useTransition to trigger the action. This should ensure that the data stays in the fields to be updated

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -8,13 +8,21 @@ import {
   PopoverTrigger,
   PopoverContent,
 } from '@nextui-org/react';
-import { useActionState } from 'react';
+import { useActionState, useTransition } from 'react';
 import * as actions from '@/actions';
 
 export default function TopicCreateForm() {
   const [formState, action] = useActionState(actions.createTopic, {
     errors: {},
   });
+
+  const [isPending, startFormTransition] = useTransition();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    startFormTransition(() => action(formData));
+  };
 
   return (
     <Popover placement="left">
@@ -23,7 +31,7 @@ export default function TopicCreateForm() {
       </PopoverTrigger>
 
       <PopoverContent>
-        <form action={action}>
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
