104c82fc8b357ef2d3f56939ce05b4fea26b3fa2
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-25 11:47:59 +0000

    add responsiveness to columns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import TopicCreateForm from '@/components/topics/topic-create-form';
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-4 gap-4 p-4">
-      <div className="col-span-3">
+    <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 p-4">
+      <div className="col-span-1 sm:col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
       </div>
 
