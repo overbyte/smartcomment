65ae53df8f6b35c8033c3309a7feb819d3801978
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-25 12:38:39 +0000

    remove server action delay

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -33,9 +33,6 @@ export async function createTopic(
   formState: CreateTopicFormState,
   formData: FormData
 ): Promise<CreateTopicFormState> {
-  // pause server action to test isPending
-  await new Promise((resolve) => setTimeout(resolve, 2500));
-
   const result = createTopicSchema.safeParse({
     name: formData.get('name'),
     description: formData.get('description'),
