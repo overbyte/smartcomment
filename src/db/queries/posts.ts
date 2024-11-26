0aef43a12675bcf7eda6d8d9279f96f9ff2eadbf
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-26 13:10:05 +0000

    add commented alternative method to get the method return type

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -7,6 +7,16 @@ export type PostWithData = Post & {
   _count: { comments: number };
 };
 
+// alternative method of creating the type
+// use the de-promised return type from the fetchPostsByTopicSlug
+// function and take the first element
+// so Awaited = unwrap the promise
+// ReturnType = get the return type from typeof the function
+// [number] = get the first element
+// export type PostWithData = Awaited<
+//   ReturnType<typeof fetchPostsByTopicSlug>
+// >[number];
+
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
   return db.post.findMany({
     where: { topic: { slug } },
