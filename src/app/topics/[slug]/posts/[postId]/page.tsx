57c643c724c2cd86db2fe0900ec95d83788e1bf3
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-26 12:59:43 +0000

    add course files which duplicate a lot of the effort from topics

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,27 @@
-export default function Post() {
-  return <div>Post</div>;
+import Link from 'next/link';
+import PostShow from '@/components/posts/post-show';
+import CommentList from '@/components/comments/comment-list';
+import CommentCreateForm from '@/components/comments/comment-create-form';
+import paths from '@/paths';
+
+interface PostPageProps {
+  params: Promise<{
+    slug: string;
+    postId: string;
+  }>;
+}
+
+export default async function PostPage({ params }: PostPageProps) {
+  const { slug, postId } = await params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topic(slug)}>
+        {'< '}Back to {slug}
+      </Link>
+      {/* <Post /> */}
+      {/* <CommentCreateForm postId={postId} startOpen /> */}
+      {/* <CommentList comments={comments} /> */}
+    </div>
+  );
 }
