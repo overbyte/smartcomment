326cbef8a2dddc04cd4d54aac3d9df2f71659b03
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-27 11:32:12 +0000

    fix screen height using min-h-screen instead of h-screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <Providers>
-          <div className="container h-screen flex flex-col mx-auto px-4 max-w-6xl">
+          <div className="container min-h-screen flex flex-col mx-auto px-4 max-w-6xl">
             <Header />
             <main>{children}</main>
             <footer className="mt-auto mx-auto p-4 max-w-6xl">
