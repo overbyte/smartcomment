fef1c34e8b2b3cfa2d5ccfdb5e9db1044026c6e5
Allandt Bik-Elliott <allandt@gmail.com>
2024-12-06 13:10:31 +0000

    add top 5 posts by comment to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Divider } from '@nextui-org/react';
+import { fetchTopPosts } from '@/db/queries/posts';
 import TopicCreateForm from '@/components/topics/topic-create-form';
 import TopicList from '@/components/topics/topic-list';
+import PostList from '@/components/posts/post-list';
 
 export default function Home() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 p-4">
       <div className="col-span-1 sm:col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
+        <PostList fetchData={fetchTopPosts} />
       </div>
 
       <div className="border shadow py-3 px-2">
